90bed85263ad43f1c5d4c8a288a19efd6f3f4776
Mario Roaf-Esparza <marioroafesparza@gmail.com>
2016-07-01 15:29:24 -0700

    clean up

---
+++ server/config/middleware.js
@@ -0,0 +1,20 @@
+//var routes = require('./routes.js');
+var bodyParser = require('body-parser');
+var request = require('request');
+
+
+
+module.exports = function (app, express) {
+  
+  //route for client
+  var userRouter = express.Router();
+  var questionRouter = express.Router();
+
+
+  //parse json
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+
+  //serve index
+  app.use(express.static(__dirname + '/../../client'));
+};
