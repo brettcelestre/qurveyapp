3ccd428331a9bb70c04a8da6f3416c0307d1c9a4
Mario Roaf-Esparza <marioroafesparza@gmail.com>
2016-07-06 15:51:19 -0700

    signup adds user to db

--- server/answers/answersCtrl.js
+++ server/answers/answersCtrl.js
@@ -1,8 +1,36 @@
+var Answer = require('./answersModel.js');
+var Question = require('../questions/questionsModel.js');
+var User = require('../users/usersModel.js');
+
+
 module.exports = {
   allAnswers: function(req, res) {
-    res.send('allAnswers');
+    Answer.find({})
+    .populate('user')
+    .populate('question')
+    .exec(function(err, Answers) {
+      res.send(Answers);   
+    });
   },
   newAnswer: function(req, res) {
-    res.send('newAnswer');
+    // TODO get question that has been aswered
+    Question.findOne({}, function (err, Question) {
+      req.body.question = Question._id;
+      // TODO get user that aswered question -- Session obj?
+      User.findOne({}, function(err, User) {
+        req.body.user = User._id;
+        // END TODOS
+        var newAnswer = new Answer(req.body);
+        newAnswer.save(function(err, newAnswer) {
+          if (err) {
+            res.send(err);
+            return console.error(err);
+          }
+
+          res.send(newAnswer);
+        });
+      });
+    });
+    // res.send('newAnswer');
   }
-}
+};
