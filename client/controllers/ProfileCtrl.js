00163e397cd38efee4ceab40678423c4950b72d2
Brett Celestre <bcelestre@gmail.com>
2016-07-13 15:01:49 -0700

    (feat) Updates profile page

--- client/controllers/ProfileCtrl.js
+++ client/controllers/ProfileCtrl.js
@@ -3,12 +3,35 @@ angular.module('qurvey.controllers')
 
 .controller('ProfileController', function($scope, $state, Main) {
   
-  // $scope.user = 'Main.userObject.username;
+  $scope.init = function(){
+    Main.currentUser()
+      .then(function(){
+        $scope.user = Main.userObject.username;
+        $scope.traits = Main.userObject.traits;
+        $scope.userQuestionTotal = Object.keys(Main.userObject.questionsAsked).length;
+        $scope.userAnswerTotal = Object.keys(Main.userObject.questionsAnswered).length;
+      })
+      .catch(function(data){
+        console.error('Error with login: ', data)
+      });
+  }();
   
-  // console.log('Main.userObject.username: ', Main.userObject.username);
+  $scope.user = Main.userObject.username;
+  $scope.traits = Main.userObject.traits;
+  $scope.userQuestionTotal = Object.keys(Main.userObject.questionsAsked).length;
+  $scope.userAnswerTotal = Object.keys(Main.userObject.questionsAnswered).length;
+  
+  console.log('Main.userObject.username PROFILE: ', Main.userObject.username);
   
   //Main.currentUser.username;
   
-
+  $scope.userQuestions = function(){
+    console.log('userQuestions function ran');
+  };
+  
+  $scope.userAnswers = function(){
+    console.log('userAnswers function ran');
+    
+  };
 
 });
