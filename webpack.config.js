5754fa463a2ee653f58af1f0aa38121895046c24
Brett Celestre <bcelestre@gmail.com>
2016-07-19 11:09:04 -0700

    (feat) Updates webpack config for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 
 var path    = require('path');
 var webpack = require('webpack');
+var nodeModulesPath = path.resolve(__dirname, 'node_modules');
+var buildPath = path.resolve(__dirname, 'client', 'build');
 
 module.exports = {
   context: __dirname + '/client',
   entry: './app.js',
   
   output: {
-    path: path.join(__dirname, 'client'),
+    // path: path.join(__dirname, 'client'),
+    path: buildPath,
     filename: 'bundle.js',
     publicPath: '/build/' //the server will listen in on this path and then proxy Webpack
   },
