717113fba25c4b9e6da7aa3b110bcc937dad1c6b
Brett Celestre <bcelestre@gmail.com>
2016-07-19 11:25:24 -0700

    (feat) Updates webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,16 +7,13 @@ var buildPath = path.resolve(__dirname, 'client');
 module.exports = {
   context: __dirname + '/client',
   entry: './app.js',
-  
   output: {
     // path: path.join(__dirname, 'client'),
     path: buildPath,
     filename: 'bundle.js',
     publicPath: '/build/' //the server will listen in on this path and then proxy Webpack
   },
-  
   module: {
     loaders: []
   }
-  
 };
